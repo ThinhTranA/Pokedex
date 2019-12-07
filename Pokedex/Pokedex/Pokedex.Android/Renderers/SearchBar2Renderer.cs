44ebb6facf37da7f27243f728c3a5df81bdbeb42
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-12-07 11:03:13 +0800

    Add Search bar render to remove underline

---
+++ Pokedex/Pokedex/Pokedex.Android/Renderers/SearchBar2Renderer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Pokedex.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(SearchBar), typeof(SearchBar2Renderer))]
+
+namespace Pokedex.Droid.Renderers
+{
+    public class SearchBar2Renderer : SearchBarRenderer
+    {
+        [Obsolete]
+        public SearchBar2Renderer()
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+
+            base.OnElementChanged(e);
+
+            if (Control == null || e.NewElement == null) return;
+
+            var plateId = Resources.GetIdentifier("android:id/search_plate", null, null);
+            var plate = Control.FindViewById(plateId);
+            plate.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
