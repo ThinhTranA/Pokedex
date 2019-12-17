ba7165c4e50bbd3ef8461fa5867dc4e7b7b80175
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-12-17 19:50:11 +0800

    Add TabHostView and Items

---
+++ Pokedex/Pokedex/Pokedex/RenderedViews/ShadowBoxView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Pokedex.RenderedViews
+{
+    public enum ShadowType
+    {
+        None = 0,
+        Top,
+        Bottom,
+    }
+
+    public class ShadowBoxView : BoxView
+    {
+        public static readonly BindableProperty ShadowTypeProperty = BindableProperty.Create(
+            nameof(ShadowType),
+            typeof(ShadowType),
+            typeof(ShadowBoxView));
+
+        public ShadowType ShadowType
+        {
+            get => (ShadowType)GetValue(ShadowTypeProperty);
+            set => SetValue(ShadowTypeProperty, value);
+        }
+    }
+}
